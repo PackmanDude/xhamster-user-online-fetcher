b1cd188bcf2297b32354486331f4e898e56bb99d
PackmanDude <packmandude@protonmail.com>
2022-08-22 12:44:34 +0300

    Update

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
+// Required configuration:
+//	1. set user variable below;
+//	2. paste
+//	<script>let container = document.getElementById('graph');</script>
+//	before importing this to your HTML file.
+
 const user = 'coffeewithcream'
+
+const vis = require('vis-timeline')
+const options =
+{
+  style: 'bar',
+  format:
+  {
+    majorLabels:
+    {
+      millisecond: 'h:mm:ss'
+    }
+  }
+}
 
 const Http = new XMLHttpRequest()
 const url = `https://xhamster.com/users/${user}`
@@ -6,23 +25,58 @@ Http.responseType = 'document'
 Http.open('GET', url)
 Http.send()
 
+let items = []
+
 function isOnline()
 {
 	const _parent = document.getElementsByClassName('details-row')[0]
 
 	if (_parent.querySelector('#online') != null)
 	{
-		console.log('sus amongus!')
-		return true
+		return 1
 	}
 
 	if (_parent.querySelector('#offline') != null)
 	{
-		console.log('no sus!')
-		return false
+		return 0
 	}
-	console.log('unexpected error occured')
-	return false
+	console.log('unexpected error occured.')
+	return 2
 }
 
-isOnline()
+let graph = new vis.Graph2d(container, items, options)
+
+(async () =>
+{
+	while (true)
+	// do
+	{
+		await new Promise(f => setTimeout(f, 60000))
+		let result = isOnline()
+
+		if (!result && result)
+		{
+			if (!result)
+			{
+				let item =
+				{
+					x: new Date(),
+					y: 0
+				}
+				continue
+			}
+
+			let item =
+			{
+				x: new Date(),
+				y: 1
+			}
+			graph.itemsData.getDataSet().add(item)
+		}
+		// debug
+		else
+		{
+			console.log('error at async func.')
+		}
+	}
+})()
